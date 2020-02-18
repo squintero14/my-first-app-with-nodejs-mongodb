f1d2b7582b7ee8b1b62b0a9c62092f306e055a98
Sebastian Quintero <49209565+squintero14@users.noreply.github.com>
2020-02-17 20:11:40 -0500

    changing localhost

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connecting to db 
-mongoose.connect('mongodb://localhost/crud-mongo')
+mongoose.connect('mongodb://192.168.56.3/crud-mongo')
     .then(db => console.log('Db connected'))
     .catch(err => console.log(err));
 
